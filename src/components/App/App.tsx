2aaaf178f89c48a96ed5d8538fe313029c889919
Depressed <dima6493907@gmail.com>
2024-08-24 10:12:53 +0300

    add new Feature

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import Header from "../Header/Header";
+import styles from "./App.module.css";
+
 const App = () => {
-  return <div>app</div>;
+  const [count, setCount] = useState<number>(1);
+
+  const buttonHandlerPlus = () => {
+    setCount(count + 1);
+  };
+  const buttonHandlerMinus = () => {
+    setCount(count - 1);
+  };
+  const buttonHandler = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className={styles.app}>
+      <div className={styles.wrapper}>
+        <Header />
+        <div className={styles.content}>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, exercitationem dicta aliquam repudiandae iste
+            placeat modi reiciendis pariatur molestias amet, neque natus ipsum distinctio mollitia?
+          </div>
+          <div>Count:{count}</div>
+          <div>
+            <button onClick={buttonHandlerPlus}>press F</button>
+            <button onClick={buttonHandlerMinus}>press D</button>
+            <button onClick={buttonHandler}>press R</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
